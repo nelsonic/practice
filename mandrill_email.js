e5629570b809568ef1091e4895d84daa5990a2ee
nelsonic <contact.nelsonic@gmail.com>
2015-04-17 21:53:00 +0100

    almost ready. https://github.com/ideaq/time/issues/135

--- mandrill_email.js
+++ mandrill_email.js
@@ -1,37 +1,40 @@
-var mandrill    = require('mandrill-api');
 var fs         = require('fs');
 var template   = fs.readFileSync('./email_html_template.html', 'utf8');
 var textonly   = fs.readFileSync('./email_text_template.txt', 'utf8');
 
+var mandrill    = require('mandrill-api');
 mandrill_client = new mandrill.Mandrill(process.env.MANDRILL_APIKEY);
 
-var message = {
-    "html": template,
-    "text": textonly,
-    "subject": "Welcome to DWYL",
-    "from_email": "hello@dwyl.io",
-    "from_name": "Hello from DWYL",
-    "to": [{
-            "email": "dwyl.smith@gmail.com",
-            // "name": "FirstName", // not using this for now.
-            "type": "to"
-        }],
-    "headers": {
-        "Reply-To": "hello@dwyl.io"
-    },
-    "important": false,
-    "track_opens": true,
-    "track_clicks": true,
-    "tags": [
-        "registration"
-    ],
-};
+module.exports = function email(person, callback) {
+    var message = {
+        "html": template,
+        "text": textonly,
+        "subject": "Welcome to DWYL",
+        "from_email": "hello@dwyl.io",
+        "from_name": "Hello from DWYL",
+        "to": [{
+                "email": person.email,
+                // "name": "FirstName", // not using this for now.
+                "type": "to"
+            }],
+        "headers": {
+            "Reply-To": "hello@dwyl.io"
+        },
+        "important": false,
+        "track_opens": true,
+        "track_clicks": true,
+        "tags": [
+            "registration"
+        ],
+    };
 
-mandrill_client.messages.send({"message": message}, function(result) {
-    console.log(result);
+    mandrill_client.messages.send({"message": message}, function(result) {
+        console.log(result);
+        callback(result);
+    }, function(error) {
+        // Mandrill returns the error as an object with name and message keys
+        console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+        // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+    });
 
-}, function(e) {
-    // Mandrill returns the error as an object with name and message keys
-    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
-    // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
-});
+}
