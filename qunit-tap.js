f8f3539b9fc3e088f1b2b925f8022164628a5024
nelsonic <contact.nelsonic@gmail.com>
2015-05-05 16:58:28 +0100

    inline quint-tap require/boot and adds comments

--- qunit-tap.js
+++ qunit-tap.js
@@ -1,9 +1,9 @@
-var QUnit    = require('qunitjs');
-var qunitTap = require('qunit-tap');
-qunitTap(QUnit, console.log);
+var QUnit    = require('qunitjs');        // require QUnit and all its friends
+require('qunit-tap')(QUnit, console.log); // tell qunit-tap to use console.log for test output
 
+// write a few tests
 test("true is true", function() {
     equal( true, true, "message goes here");
 });
 
-QUnit.load();
+QUnit.load(); // run our test suite.
