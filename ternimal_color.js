4f1280a56bdbdb9fb6ecc6664a17fb91bc152b24
Nelson <contact.nelsonic@gmail.com>
2015-08-28 20:43:51 +0100

    got full-line colored output working. not pretty but functional

--- ternimal_color.js
+++ ternimal_color.js
@@ -1,7 +1,14 @@
 var ansi = require('ansi-styles');
 console.log(ansi.bgYellow.open + ansi.black.open + ' Hello world! ' +ansi.black.close + ansi.bgYellow.close);
 
-var open  = '\u001b[43m' + '\u001b[30m'
-var close = '\u001b[49m' + '\u001b[39m'
+var cb  = '\u001b[46m' + '\u001b[30m'; // Background Cyan Font Color Black
+var gb  = '\u001b[42m' + '\u001b[30m'; // Background Green Font Color Black
+var end = '\u001b[49m' + '\u001b[39m'
+// console.log('' + bgGreenBlack)
+// console.log(bgCyanBlack + ' Hello! ' + bgGreenBlack)
+// console.log(bgGreenBlack + ' HAI! ' +end + end)
 
-console.log(open + ' HAI! ' +close)
+var str   = '' + cb +'\n' + cb + ' Hello!'
+str = str + '' + gb +'\n' + gb + ' Everything is Awesome '
+str = str + gb + '' +end
+console.log(str)
