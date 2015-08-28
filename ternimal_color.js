9042147d0274ba84d25f212f8a3151f865a1c856
Nelson <contact.nelsonic@gmail.com>
2015-08-28 19:33:43 +0100

    disected https://github.com/chalk/ansi-styles/blob/master/index.js for *just* the styles we want

--- ternimal_color.js
+++ ternimal_color.js
@@ -1,2 +1,7 @@
 var ansi = require('ansi-styles');
 console.log(ansi.bgYellow.open + ansi.black.open + ' Hello world! ' +ansi.black.close + ansi.bgYellow.close);
+
+var open  = '\u001b[43m' + '\u001b[30m'
+var close = '\u001b[49m' + '\u001b[39m'
+
+console.log(open + ' HAI! ' +close)
