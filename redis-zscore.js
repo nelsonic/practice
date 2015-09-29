9bca705b6c443952c5e9fcc83366727ed287c89c
Nelson <contact.nelsonic@gmail.com>
2015-09-29 16:57:52 +0100

    adds sample script to test ZADD, ZSCORE & ZCARD for https://github.com/NodeRedis/node_redis/issues/872

---
+++ redis-zscore.js
@@ -0,0 +1,32 @@
+var redis_config =  {
+  port: 6379,
+  host: '127.0.0.1',
+  auth: '' // no password on localhost or travis
+}
+var redis = require('redis');
+var redisClient = redis.createClient(redis_config.port, redis_config.host);
+redisClient.auth(redis_config.auth);
+
+redisClient.set('redis', 'LOCAL');
+redisClient.get('redis', function (err, reply) {
+  console.log(reply.toString())
+  // t.equal(reply.toString(), 'LOCAL', '✓ LOCAL Redis is ' +reply.toString());
+  // t.end();
+});
+
+/**** TESTING http://redis.io/commands/ZSCORE + ZCARD ****/
+
+var url = 'https://github.com/dwyl/learn-redis';
+var now   = Date.now();
+var args = ['myzset', now, url];
+console.log(args);
+redisClient.zadd(args, function (err, data) {
+  console.log(err, data);
+  redisClient.zscore('myzset', url, function(err, data) {
+    console.log(" - - - - -  zscore: "+data)
+    redisClient.zcard('myzset', function(err, data){
+      console.log(" - - - - -  zscore: "+data)
+      redisClient.end();
+    });
+  });
+});
--- ternimal_color.js
+++ ternimal_color.js
@@ -3,10 +3,7 @@ console.log(ansi.bgYellow.open + ansi.black.open + ' Hello world! ' +ansi.black.
 
 var cb  = '\u001b[46m' + '\u001b[30m'; // Background Cyan Font Color Black
 var gb  = '\u001b[42m' + '\u001b[30m'; // Background Green Font Color Black
-var end = '\u001b[49m' + '\u001b[39m'
-// console.log('' + bgGreenBlack)
-// console.log(bgCyanBlack + ' Hello! ' + bgGreenBlack)
-// console.log(bgGreenBlack + ' HAI! ' +end + end)
+var end = '\u001b[49m' + '\u001b[39m'; // resume original terminal colors
 
 var str   = '' + cb +'\n' + cb + ' Hello!'
 str = str + '' + gb +'\n' + gb + ' Everything is Awesome '
