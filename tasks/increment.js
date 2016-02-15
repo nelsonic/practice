f36ea810d0271efafc80d04206a7f8d664786076
nelsonic <contact.nelsonic+github@gmail.com>
2016-02-15 20:19:41 +0000

    tasks are still run sequentially when output is async! win https://github.com/nelsonic/practice/issues/4

--- tasks/increment.js
+++ tasks/increment.js
@@ -2,8 +2,10 @@ var fs = require('fs');
 var path = require('path');
 var filepath = path.resolve(__dirname + '/counter.txt');
 // console.log(filepath)
-var file = fs.readFileSync(filepath, 'utf8');
-var counter = parseInt(file, 10) || 0;
-counter++;
-fs.writeFileSync(filepath, counter);
-console.log(counter);
+var file = fs.readFile(filepath, 'utf8', function(err, data){
+  var counter = parseInt(data, 10) || 0;
+  counter++;
+  fs.writeFile(filepath, counter, function(err){
+     console.log(counter);
+  });
+});
