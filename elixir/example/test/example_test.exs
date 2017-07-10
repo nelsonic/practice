36ac1ce01ee375896b3134fafc4cb11dbba8dc96
nelsonic <contact.nelsonic+github@gmail.com>
2017-07-11 00:22:47 +0100

    adds test setup for https://github.com/nelsonic/practice/issues/10#issuecomment-314271353

--- elixir/example/test/example_test.exs
+++ elixir/example/test/example_test.exs
@@ -2,8 +2,12 @@ defmodule ExampleTest do
   use ExUnit.Case
   doctest Example
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup_all do
+    {:ok, number: 2}
+  end
+
+  test "the truth", state do
+    assert 1 + 1 == state[:number]
   end
 end
 
@@ -19,5 +23,15 @@ defmodule TestReceive do
   test "receives ping" do
     SendingProcess.run(self())
     assert_received :ping
+  end
+end
+
+
+defmodule OutputTest do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  test "outputs Hello World" do
+    assert capture_io(fn -> IO.puts "Hello World" end) == "Hello World\n"
   end
 end
