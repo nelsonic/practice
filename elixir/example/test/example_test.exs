b64ca5eafe53d3a7992796279737e7b7c020260f
nelsonic <contact.nelsonic+github@gmail.com>
2017-07-10 23:46:06 +0100

    adds test for assert_receive for https://github.com/nelsonic/practice/issues/10#issuecomment-314271353

--- elixir/example/test/example_test.exs
+++ elixir/example/test/example_test.exs
@@ -6,3 +6,18 @@ defmodule ExampleTest do
     assert 1 + 1 == 2
   end
 end
+
+defmodule SendingProcess do
+  def run(pid) do
+    send pid, :ping
+  end
+end
+
+defmodule TestReceive do
+  use ExUnit.Case
+
+  test "receives ping" do
+    SendingProcess.run(self())
+    assert_received :ping
+  end
+end
