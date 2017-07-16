a044a7ab8934392616f8cf1b1ce9173143ff26ca
nelsonic <contact.nelsonic+github@gmail.com>
2017-07-16 21:03:52 +0100

    adds simplequeue.ex for OTP concurrency example https://github.com/nelsonic/practice/issues/10#issuecomment-315632668

---
+++ elixir/simplequeue.ex
@@ -0,0 +1,36 @@
+defmodule SimpleQueue do
+  use GenServer
+
+  ### GenServer API
+
+  @doc """
+  GenServer.init/1 callback
+  """
+  def init(state), do: {:ok, state}
+
+  @doc """
+  GenServer.handle_call/3 callback
+  """
+  def handle_call(:dequeue, _from, [value|state]) do
+    {:reply, value, state}
+  end
+  def handle_call(:dequeue, _from, []), do: {:reply, nil, []}
+
+  def handle_call(:queue, _from, state), do: {:reply, state, state}
+
+  @doc """
+  GenServer.handle_cast/2 callback
+  """
+  def handle_cast({:enqueue, value}, state) do
+    {:noreply, state ++ [value]}
+  end
+
+  ### Client API / Helper functions
+
+  def start_link(state \\ []) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+  def queue, do: GenServer.call(__MODULE__, :queue)
+  def enqueue(value), do: GenServer.cast(__MODULE__, {:enqueue, value})
+  def dequeue, do: GenServer.call(__MODULE__, :dequeue)
+end
