e8f126ef6f749e4f4c1c97f2ea38f6f687047379
nelsonic <contact.nelsonic+github@gmail.com>
2017-07-18 02:12:29 +0100

    adds @type & @spec example for https://github.com/nelsonic/practice/issues/10#issuecomment-315926306

--- elixir/example/lib/example.ex
+++ elixir/example/lib/example.ex
@@ -15,4 +15,25 @@ defmodule Example do
   def hello do
     :world
   end
+
+
+  defstruct first: nil, last: nil
+
+  @type t(first, last) :: %Example{first: first, last: last}
+
+  @typedoc """
+    Type that represents Examples struct with :first
+    as integer and :last as integer.
+  """
+  @type t :: %Example{first: integer, last: integer}
+
+  @spec sum_times(integer, %Example{first: integer, last: integer}) :: integer
+  def sum_times(a, params) do
+    for i <- params.first..params.last do
+      i
+    end
+      |> Enum.map(fn el -> el * a end)
+      |> Enum.sum
+      |> round
+  end
 end
