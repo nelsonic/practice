79344b36f93ddcb5e1164b4af6c9b39deabddf38
nelsonic <contact.nelsonic+github@gmail.com>
2017-08-18 07:37:20 +0100

    adds code snapshot at end of GenServer "chapter" of Mix & OTP for #11

---
+++ elixir/kv/test/kv/bucket_test.exs
@@ -0,0 +1,28 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    {:ok, bucket} = start_supervised(KV.Bucket)
+    %{bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "delete a key from bucket", %{bucket: bucket} do
+    # confirm the key does not exist before we create it:
+    assert KV.Bucket.get(bucket, "kale") == nil
+
+    KV.Bucket.put(bucket, "kale", 4)
+    assert KV.Bucket.get(bucket, "kale") == 4
+
+    # delete the key:
+    assert KV.Bucket.delete(bucket, "kale") === 4
+    # confirm the key no longer exists
+    assert KV.Bucket.get(bucket, "kale") == nil
+  end
+end
