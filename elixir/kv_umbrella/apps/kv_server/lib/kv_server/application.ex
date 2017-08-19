dc2cf299e427c5fadf690f43d2536c51f1e31638
nelsonic <contact.nelsonic+github@gmail.com>
2017-08-19 06:39:54 +0100

    adds code snapshot at end of "Docs, tests and with" chapter. Note: tests are failing! see: https://github.com/nelsonic/practice/issues/11#issuecomment-323457760

--- elixir/kv_umbrella/apps/kv_server/lib/kv_server/application.ex
+++ elixir/kv_umbrella/apps/kv_server/lib/kv_server/application.ex
@@ -8,8 +8,8 @@ defmodule KVServer.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
-      # Starts a worker by calling: KVServer.Worker.start_link(arg)
-      # {KVServer.Worker, arg},
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      Supervisor.child_spec({Task, fn -> KVServer.accept(4040) end}, restart: :permanent)
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
