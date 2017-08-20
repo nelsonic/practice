715b9dca0c021d60bb9e2319423a6f14f00182ce
nelsonic <contact.nelsonic+github@gmail.com>
2017-08-20 22:23:58 +0100

    add snapshot of code at end of "Mix and OTP" lesons #11

--- elixir/kv_umbrella/apps/kv/lib/kv/supervisor.ex
+++ elixir/kv_umbrella/apps/kv/lib/kv/supervisor.ex
@@ -8,7 +8,8 @@ defmodule KV.Supervisor do
   def init(:ok) do
     children = [
       {KV.Registry, name: KV.Registry},
-      KV.BucketSupervisor
+      KV.BucketSupervisor,
+      # {Task.Supervisor, name: KV.RouterTasks},
     ]
 
     Supervisor.init(children, strategy: :one_for_all)
