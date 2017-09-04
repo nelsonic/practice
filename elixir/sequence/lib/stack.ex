5391ea9909246827c4eb2ded9d5cdcb541d786f6
nelsonic <contact.nelsonic+github@gmail.com>
2017-09-04 20:38:08 +0100

    adds ExDoc to Stack example with usage in iex

--- elixir/sequence/lib/stack.ex
+++ elixir/sequence/lib/stack.ex
@@ -1,6 +1,19 @@
 defmodule Totes.McStack do
   use GenServer                      
 
+  @doc """
+   When you call the :pop action it returns the first element in the array
+
+   Returns `:hello`.
+
+   ## Examples
+
+       iex -S mix
+       iex> { :ok, pid } = GenServer.start_link(Totes.McStack, [:hello, :world, "Awesome!"])
+       iex> GenServer.call(pid, :pop)
+       :hello
+
+   """
   def handle_call(:pop, _from, stack) when stack == [] do        
     { :reply, :empty, [] }
   end
