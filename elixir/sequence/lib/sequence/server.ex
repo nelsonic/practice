ec0809dc320a8dc34c071508211e6248b16cdc52
nelsonic <contact.nelsonic+github@gmail.com>
2017-09-04 20:27:28 +0100

    adds additional callbacks from p.219 of Programming Elixir book

--- elixir/sequence/lib/sequence/server.ex
+++ elixir/sequence/lib/sequence/server.ex
@@ -5,6 +5,10 @@ defmodule Sequence.Server do
     { :reply, current_number, current_number+1 }
   end
 
+  def handle_call({:set_number, new_number}, _from, _current_number) do 
+    { :reply, new_number, new_number }
+  end
+
   def handle_cast({:increment_number, delta}, current_number) do
     { :noreply, current_number + delta}
   end
