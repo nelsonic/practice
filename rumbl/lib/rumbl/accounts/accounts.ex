4fd1cb8318bb98424346705f283aba5988f20d99
nelsonic <nelson+github@dwyl.io>
2019-01-22 22:52:42 +0000

    re-write accounts.ex to use Ecto repo. p.59 for #55

--- rumbl/lib/rumbl/accounts/accounts.ex
+++ rumbl/lib/rumbl/accounts/accounts.ex
@@ -3,23 +3,24 @@ defmodule Rumbl.Accounts do
   The Accounts context.
   """
 
-  alias Rumbl.Accounts.User 
+  alias Rumbl.Accounts.User
 
-  def list_users do
-    [
-      %User{id: "1", name: "José", username: "josevalim"},
-      %User{id: "2", name: "Bruce", username: "redrapids"},
-      %User{id: "3", name: "Chris", username: "chrismccord"}
-    ]
+  alias Rumbl.Repo
+
+  def get_user(id) do 
+    Repo.get(User, id)
   end
 
-  def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+  def get_user!(id) do 
+    Repo.get!(User, id)
   end
 
-  def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+  def get_user_by(params) do 
+    Repo.get_by(User, params)
   end
+
+  def list_users do 
+    Repo.all(User)
+  end
+
 end
