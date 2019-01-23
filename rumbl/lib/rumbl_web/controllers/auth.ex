35627e03feaac60e52950c1f4101d72027111ab3
nelsonic <nelson+github@dwyl.io>
2019-01-23 21:40:24 +0000

    add login function p.90 for #55

--- rumbl/lib/rumbl_web/controllers/auth.ex
+++ rumbl/lib/rumbl_web/controllers/auth.ex
@@ -10,4 +10,11 @@ defmodule RumblWeb.Auth do
     user = user_id && Accounts.get_user(user_id)
     assign(conn, :current_user, user)
   end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
 end
