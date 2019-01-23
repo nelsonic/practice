9d8ac3fce755d661da0ab004298919f18ded4941
nelsonic <nelson+github@dwyl.io>
2019-01-23 21:20:31 +0000

    add authenticate function to user_controller p.88 for #55

--- rumbl/lib/rumbl_web/controllers/user_controller.ex
+++ rumbl/lib/rumbl_web/controllers/user_controller.ex
@@ -31,4 +31,15 @@ defmodule RumblWeb.UserController do
         render(conn, "new.html", changeset: changeset)
     end
   end
+
+  defp authenticate(conn) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn 
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
+    end
+  end
 end
