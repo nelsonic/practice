ef018dabe0f3767dc508cb6d6f586a0bb26fbd45
nelsonic <nelson+github@dwyl.io>
2019-01-23 21:25:48 +0000

    fix typo Account >> Accounts in auth.ex see: https://github.com/nelsonic/practice/issues/55#issuecomment-456971698

--- rumbl/lib/rumbl_web/controllers/auth.ex
+++ rumbl/lib/rumbl_web/controllers/auth.ex
@@ -7,7 +7,7 @@ defmodule RumblWeb.Auth do
 
   def call(conn, _opts) do
     user_id = get_session(conn, :user_id)
-    user = user_id && Account.get_user(user_id)
+    user = user_id && Accounts.get_user(user_id)
     assign(conn, :current_user, user)
   end
 end
