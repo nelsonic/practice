ce796a6879182de3341a471cf78718f3d3ad4066
nelsonic <nelson+github@dwyl.io>
2019-01-23 21:23:05 +0000

    use authenticate(conn) function in user_controler index p.88 for #55

--- rumbl/lib/rumbl_web/controllers/user_controller.ex
+++ rumbl/lib/rumbl_web/controllers/user_controller.ex
@@ -11,8 +11,14 @@ defmodule RumblWeb.UserController do
   end
 
   def index(conn, _params) do
-    users = Accounts.list_users()
-    render(conn, "index.html", users: users)
+    case authenticate(conn) do
+      %Plug.Conn{halted: true} = conn ->
+        conn
+
+      conn ->
+        users = Accounts.list_users()
+        render(conn, "index.html", users: users)
+    end
   end
 
   def show(conn, %{"id" => id}) do
