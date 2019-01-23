9ddac1e72585ce50127addfb4c986d23384ca4e8
nelsonic <nelson+github@dwyl.io>
2019-01-23 21:46:00 +0000

    add /sessions route to router.ex p.92 for #55

--- rumbl/lib/rumbl_web/router.ex
+++ rumbl/lib/rumbl_web/router.ex
@@ -18,6 +18,7 @@ defmodule RumblWeb.Router do
     pipe_through :browser
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
   end
 
   # Other scopes may use custom stacks.
