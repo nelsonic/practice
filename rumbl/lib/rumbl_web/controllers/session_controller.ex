4016e91a5cc87aa966feb6b02be8896eb8be6b52
nelsonic <nelson+github@dwyl.io>
2019-01-23 21:49:48 +0000

    add create session function p.92 #55

--- rumbl/lib/rumbl_web/controllers/session_controller.ex
+++ rumbl/lib/rumbl_web/controllers/session_controller.ex
@@ -4,4 +4,18 @@ defmodule RumblWeb.SessionController do
   def new(conn, _) do
     render(conn, "new.html")
   end
+
+  def create(conn, %{"session" => %{"email" => email, "password" => pass}}) do
+    case RumblWeb.Auth.login_by_email_and_pass(conn, email, pass) do
+      {:ok, conn} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> redirect(to: Routes.page_path(conn, :index))
+
+      {:error, _reason, conn} ->
+        conn
+        |> put_flash(:error, "Invalid email/password combination")
+        |> render("new.html")
+    end
+  end
 end
