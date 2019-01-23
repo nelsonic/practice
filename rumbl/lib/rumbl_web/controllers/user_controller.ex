d170ab12b9f5d11cef725af6add052599801a25d
nelsonic <nelson+github@dwyl.io>
2019-01-23 20:31:15 +0000

    update user_controller to create function to use Accounts.register_user p.81 #55

--- rumbl/lib/rumbl_web/controllers/user_controller.ex
+++ rumbl/lib/rumbl_web/controllers/user_controller.ex
@@ -21,11 +21,12 @@ defmodule RumblWeb.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    case Accounts.create_user(user_params) do
+    case Accounts.register_user(user_params) do
       {:ok, user} ->     
         conn
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: Routes.user_path(conn, :index))
+
       {:error, %Ecto.Changeset{} =  changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
