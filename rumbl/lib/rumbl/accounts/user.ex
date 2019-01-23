00c1674aee8c7ac5bcee1b3d67a39e172f66fd53
nelsonic <nelson+github@dwyl.io>
2019-01-23 18:37:37 +0000

    adds registration_changeset on p.75 for #55

--- rumbl/lib/rumbl/accounts/user.ex
+++ rumbl/lib/rumbl/accounts/user.ex
@@ -5,6 +5,7 @@ defmodule Rumbl.Accounts.User do
   schema "users" do
     field :name, :string
     field :username, :string
+    has_one :credential, Credential
 
     timestamps()
   end
@@ -15,5 +16,12 @@ defmodule Rumbl.Accounts.User do
     |> validate_required([:name, :username])
     |> validate_length(:username, min: 1, max: 20)
   end
+
+  def registration_changeset(user, params) do
+    user
+    |> changeset(params)
+    |> cast_assoc(:credential, with: &Credential.changeset/2, required: true)
+  end
+
 end
 
