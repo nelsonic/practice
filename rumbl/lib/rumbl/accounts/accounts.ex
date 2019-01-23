d418dd246d09d11e36fc87a9efc429fce5d942f8
nelsonic <nelson+github@dwyl.io>
2019-01-23 21:55:47 +0000

    create authenticate_by_email_and_pass function in accounts.ex p.93 for #55

--- rumbl/lib/rumbl/accounts/accounts.ex
+++ rumbl/lib/rumbl/accounts/accounts.ex
@@ -134,4 +134,29 @@ defmodule Rumbl.Accounts do
   def change_credential(%Credential{} = credential) do
     Credential.changeset(credential, %{})
   end
+
+  import Ecto.Query
+
+  def get_user_by_email(email) do
+    from(u in User, join: c in assoc(u, :credential), where: c.email == ^email)
+    |> Repo.one()
+    |> Repo.preload(:credential)
+  end
+
+  def authenticate_by_email_and_pass(email, given_pass) do
+    user = get_user_by_email(email)
+
+    cond do
+      user && Comeonin.Pbkdf2.checkpw(given_pass, user.credential.password_hash) ->
+        {:ok, user}
+
+      user ->
+        {:error, :unauthorized}
+
+      true ->
+        Comeonin.Bcrypt.dummy_checkpw()
+        {:error, :not_found}
+    end
+  end
+
 end
