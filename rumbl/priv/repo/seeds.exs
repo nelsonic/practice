dad5181135fd14fa4c0eefbd489193d253e1ed77
nelsonic <nelson+github@dwyl.io>
2019-01-24 14:02:38 +0000

    add seeds function to create categories p.124 #55

--- rumbl/priv/repo/seeds.exs
+++ rumbl/priv/repo/seeds.exs
@@ -9,3 +9,8 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Rumbl.Multimedia
+
+for category <- ~w(Action Drama Romance Comedy Sci-fi) do
+  Multimedia.create_category(category)
+end
