7efc68e37350d8fee572974bb706cb1d15993e07
nelsonic <nelson+github@dwyl.io>
2019-01-24 11:27:47 +0000

    update video_controller to use the "current_user" p.115 #55

--- rumbl/lib/rumbl_web/controllers/video_controller.ex
+++ rumbl/lib/rumbl_web/controllers/video_controller.ex
@@ -9,13 +9,13 @@ defmodule RumblWeb.VideoController do
     render(conn, "index.html", videos: videos)
   end
 
-  def new(conn, _params) do
-    changeset = Multimedia.change_video(conn.assigns.current_user, %Video{}) 
+  def new(conn, _params, current_user) do
+    changeset = Multimedia.change_video(current_user, %Video{})
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"video" => video_params}) do
-    case Multimedia.create_video(conn.assigns.current_user, video_params) do 
+  def create(conn, %{"video" => video_params}, current_user) do
+    case Multimedia.create_video(current_user, video_params) do
       {:ok, video} ->
         conn
         |> put_flash(:info, "Video created successfully.")
