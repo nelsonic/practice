a7947ae10fa230b3284f7e4a56ad6e26ae0d8685
nelsonic <nelson+github@dwyl.io>
2019-01-24 13:24:58 +0000

    add video categories schema & migration p.122 #55

---
+++ rumbl/lib/rumbl/multimedia/category.ex
@@ -0,0 +1,18 @@
+defmodule Rumbl.Multimedia.Category do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "categories" do
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(category, attrs) do
+    category
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+  end
+end
