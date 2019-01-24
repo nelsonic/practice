0ea76f4615a280a09ab3dc36718f819266bc2d7c
nelsonic <nelson+github@dwyl.io>
2019-01-24 11:03:51 +0000

    adds "belongs_to :user, Rumbl.Accounts.User" association in video.ex p.110 for #55

--- rumbl/lib/rumbl/multimedia/video.ex
+++ rumbl/lib/rumbl/multimedia/video.ex
@@ -7,7 +7,7 @@ defmodule Rumbl.Multimedia.Video do
     field :description, :string
     field :title, :string
     field :url, :string
-    field :user_id, :id
+    belongs_to :user, Rumbl.Accounts.User
 
     timestamps()
   end
