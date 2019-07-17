4cf6440bcbb67bb5c9aaaffa4b5c38ef650325da
nelsonic <nelson+github@dwyl.io>
2019-07-17 06:54:31 +0100

    update version of Phoenix in mix.exs to latest (1.4.9) for https://github.com/nelsonic/practice/issues/55#issuecomment-511127182

--- rumbl/mix.exs
+++ rumbl/mix.exs
@@ -33,20 +33,16 @@ defmodule Rumbl.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.0"},
+      {:phoenix, "~> 1.4.9"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
-      {:ecto_sql, "~> 3.0"},
+      {:ecto_sql, "~> 3.1"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-      # auth:
-      {:comeonin, "~> 4.1"}, 
-      {:bcrypt_elixir, "~> 1.0"}, 
-      {:pbkdf2_elixir, "~> 0.12"},
+      {:plug_cowboy, "~> 2.0"}
     ]
   end
 
